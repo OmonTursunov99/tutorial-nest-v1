21bafd1b53e983f32bdabe55979dc49b5a3a62b5
Mon <94193911+OmonTursunov99@users.noreply.github.com>
2023-08-30 14:21:40 +0500

    [8] remove: http server

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,10 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app/app.module';
-//
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   await app.listen(3000);
-//
-//   console.info("Bootstrap app");
-// }
-// bootstrap();
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import serverless = require('serverless-http');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = '.netlify/functions/main';
-  app.setGlobalPrefix(globalPrefix);
-  await app.init();
+  await app.listen(3000);
 
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverless(expressApp)
+  console.info("Bootstrap app");
 }
-
-let server;
-export const handler = async (event, context, callback) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
+bootstrap();
