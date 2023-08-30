5c513dbee835e0d43ba191b97a5c89cb7eb2c533
Mon <94193911+OmonTursunov99@users.noreply.github.com>
2023-08-30 14:17:50 +0500

    [7] feat: serverless-http

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app/app.module';
+//
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+//
+//   console.info("Bootstrap app");
+// }
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import serverless = require('serverless-http');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = '.netlify/functions/main';
+  app.setGlobalPrefix(globalPrefix);
+  await app.init();
 
-  console.info("Bootstrap app");
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp)
 }
-bootstrap();
+
+let server;
+export const handler = async (event, context, callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
